4ab43186e9c31ab73597248d9e5b705668bddf01
bgibz <brendanjamesgibson@gmail.com>
2020-10-30 18:33:22 -0700

    misnamed some properties

--- _starter/inc/ajax.php
+++ _starter/inc/ajax.php
@@ -10,14 +10,14 @@
  add_action( 'wp_ajax_brewsite_submit_contact_form', 'brewsite_submit_contact_form');
 
  function brewsite_submit_contact_form() {
-     $title = wp_strip_all_tags($_POST["title"]);
+     $title = wp_strip_all_tags($_POST["name"]);
      $name =wp_strip_all_tags( $_POST["name"]);
      $email = wp_strip_all_tags($_POST["email"]);
      $message = wp_strip_all_tags($_POST["message"]);
 
      $args = array(
          'post_title' => $title,
-         'post_message' => $message,
+         'post_content' => $message,
          'post_author' => 1,
          'post_status' => 'publish',
          'post_type' => 'brewsite_contact',
